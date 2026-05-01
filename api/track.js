365021491c883d63feb11f0d30b72b94199da663
Waycel.LLC <manager@waycel.com>
2026-05-01 14:00:13 -0300

    Update track.js

--- api/track.js
+++ api/track.js
@@ -1,4 +1,14 @@
 export default async function handler(req, res) {
+  // CORS headers (ESTO SOLUCIONA TODO)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Responder preflight request
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method === 'POST') {
     console.log("EVENTS RECEIVED:");
     console.log(JSON.stringify(req.body, null, 2));
