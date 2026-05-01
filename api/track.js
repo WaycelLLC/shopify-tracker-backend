4b93f73d51fdd5148707c3c747e64cf9f190a4dc
Waycel.LLC <manager@waycel.com>
2026-05-01 18:47:53 -0300

    Update track.js

--- api/track.js
+++ api/track.js
@@ -1,17 +1,28 @@
 export default async function handler(req, res) {
-  // CORS headers (ESTO SOLUCIONA TODO)
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  // Responder preflight request
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
 
   if (req.method === 'POST') {
-    console.log("EVENTS RECEIVED:");
-    console.log(JSON.stringify(req.body, null, 2));
+    const { event, url, timestamp } = req.body;
+
+    await fetch('https://cvmkonpbmpubskobzbqc.supabase.co/rest/v1/events', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2bWtvbnBibXB1YnNrb2J6YnFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2NTA4NTAsImV4cCI6MjA5MzIyNjg1MH0.XQRFTqzIVVCWAs6OXcYxtKK4CkjH37H7WiXLk-gfYD8',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2bWtvbnBibXB1YnNrb2J6YnFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2NTA4NTAsImV4cCI6MjA5MzIyNjg1MH0.XQRFTqzIVVCWAs6OXcYxtKK4CkjH37H7WiXLk-gfYD8'
+      },
+      body: JSON.stringify({
+        event,
+        url,
+        timestamp
+      })
+    });
 
     return res.status(200).json({ ok: true });
   }
